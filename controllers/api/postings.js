444781328e942602bd5ed5dfdbfbf3f39b4cccbd
Thomas Schild <thomas@schilds.net>
2015-06-17 09:56:00 +0200

    refactoring: move postings-api out of server.js

---
+++ controllers/api/postings.js
@@ -0,0 +1,38 @@
+/* jslint node: true */
+'use strict';
+
+var express = require('express');
+var Posting = require('../..//models/posting');
+
+var router = express.Router();
+
+router.get('/api/postings', function(req, res, next) {
+	console.log('req GET to "api/postings"');
+
+	Posting.find().sort('-date').exec(function(err, postings) {
+		if (err) {
+			return next(err);
+		}
+		res.json(postings);
+		console.log('... res: ' + res.statusMessage);
+	});
+});
+
+router.post('/api/postings', function(req, res, next) {
+	console.log('req POST to "api/postings"');
+
+	var posting = new Posting({
+		username: req.body.username,
+		content: req.body.content
+	});
+
+	posting.save(function(err, posting) {
+		if (err) {
+			return next(err);
+		}
+		res.status(201).json(posting);
+		console.log('... res: ' + res.statusMessage);
+	});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,45 +3,17 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var Posting = require('./models/posting');
+var postingsApi = require('./controllers/api/postings');
 
 var portNr = 3001;
 
 var app = express();
-
 app.use(bodyParser.json());
+
+app.use(postingsApi);
 
 app.get('/', function(req, res) {
 	res.sendfile('layouts/postings.html');
-});
-
-app.get('/api/postings', function(req, res, next) {
-	console.log('req GET to "api/postings"');
-
-	Posting.find().sort('-date').exec(function(err, postings) {
-		if (err) {
-			return next(err);
-		}
-		res.json(postings);
-		console.log('... res: ' + res.statusMessage);
-	});
-});
-
-app.post('/api/postings', function(req, res, next) {
-	console.log('req POST to "api/postings"');
-
-	var posting = new Posting({
-		username: req.body.username,
-		content: req.body.content
-	});
-
-	posting.save(function(err, posting) {
-		if (err) {
-			return next(err);
-		}
-		res.status(201).json(posting);
-		console.log('... res: ' + res.statusMessage);
-	});
 });
 
 app.listen(portNr, function() {
