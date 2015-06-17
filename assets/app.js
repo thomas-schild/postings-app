b60efbeecb194a1a4a0f6713fa4c2794d50a4613
Thomas Schild <thomas@schilds.net>
2015-06-17 12:14:25 +0200

    refactorings on postingsSrv/-Ctrl

--- assets/app.js
+++ assets/app.js
@@ -4,44 +4,36 @@
 var app = angular.module('postingsApp', []);
 
 app.service('postingsSrv', function($http) {
-	var listPostings = function() {
+	this.listPostings = function() {
 		return $http.get('/api/postings');
 	};
 
-	var addPosting = function(postingContent) {
+	this.addPosting = function(postingContent) {
 		if (postingContent) {
 			var newPosting = { username: 'Poster', content: postingContent };
 			return $http.post('/api/postings', newPosting);
 		}
 		// TODO: consider what to return / how to build an empty promise obj
 	};
-
-	this.fetch = listPostings;
-	this.create = addPosting;
 });
 
 app.controller('postingsCtrl', function($scope, postingsSrv) {
 
-	var listPostings = function() {
-		postingsSrv.fetch()
-		.success(function(postings) {
-			$scope.postings = (postings) ? postings : [];
-		})
-		.error(function(err) {
-			console.log('error on listPostings(): ' + err);
-		});
+	postingsSrv.listPostings()
+	.success(function(postings) {
+		$scope.postings = (postings) ? postings : [];
+	})
+	.error(function(err) {
+		console.log('error on listPostings(): ' + err);
+	});
+
+	$scope.addPosting = function() {
+		if ($scope.postingContent) {
+			postingsSrv.addPosting($scope.postingContent)
+			.success(function(posting) {
+				$scope.postings.unshift(posting);
+				$scope.postingContent = null;
+			});
+		}
 	};
-
-	var addPosting = function() {
-		postingsSrv.create($scope.postingContent)
-		.success(function(posting) {
-			$scope.postings.unshift(posting);
-		});
-		$scope.postingContent = null;
-	};
-
-	$scope.listPostings = listPostings;
-	$scope.addPosting = addPosting;
-
-	listPostings();
 });
