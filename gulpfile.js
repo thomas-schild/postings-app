ef4f89aba8b9545015d24a33e4cb9f01815dc6c3
Thomas Schild <thomas@schilds.net>
2015-06-18 11:30:38 +0200

    added ngAnnotate and uglify for js minification compatibe with ng

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
 
 // gulp.task('default', ['sayHello'], function() {
 // 	console.log('running default task');
@@ -15,5 +17,7 @@ var concat = require('gulp-concat');
 gulp.task('buildJs', function() {
 	gulp.src(['ng/module.js', 'ng/**/*.js'])
 	.pipe(concat('app.js'))
+	.pipe(ngAnnotate())
+	.pipe(uglify())
 	.pipe(gulp.dest('assets'));
 });
