6b0107d6cc62cd0154745c57b0fdcf55fb5005f3
Thomas Schild <thomas@schilds.net>
2015-06-30 15:33:58 +0200

    ongoing work on account service

--- ng/account.svc.js
+++ ng/account.svc.js
@@ -13,11 +13,26 @@
 		svc.getAccount = getAccount;
 
 		function login(name, password) {
-			console.log('login is called, TODO: implement!');
+			console.log('accountSvc.login is called...');
+
+			var reqData = { login: name, password: password };
+			// $http-methods return promises, 
+			// use promise.then( function(res) {}, function(err) {}, function (progess) {} ) to handle
+			console.log('... sending POST req to "api/sessions" ...');
+			$http.post('api/sessions', reqData)
+			.then( function(res) {
+				console.log('... accountSvc received res: ' + res);
+				// attributes of the response object: res.status, res.statusText, res.headers, res.data, res.config
+				// get token out of response and store it within the service instance
+				svc.token = res.data;
+
+				return getAccount();
+			});
 		}
 
 		function getAccount() {
-			console.log('getAccount is called, TODO: implement!');
+
+			console.log('accountSvc.getAccount is called, TODO: implement!');
 		}
 	}
 })();
